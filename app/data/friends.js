9bcbe3e2d12117ae79d54b5e93428b835a787bdb
Craig <corsi007@umn.edu>
2018-06-14 14:22:17 -0500

    initial site files

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var storedFriends = [
+    {
+        "name":"Captain Falcon",
+        "photo":"https://vignette.wikia.nocookie.net/ssb/images/3/34/Captainfalcon_Artwork.jpg",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        "name":"Donkey Kong",
+        "photo":"https://www.ssbwiki.com/images/5/5f/Donkey_Kong_SSB4.png",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        "name":"Fox",
+        "photo":"https://www.ssbwiki.com/images/7/74/Fox_SSB4.png",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        "name":"Jigglypuff",
+        "photo":"https://www.ssbwiki.com/images/a/a6/Jigglypuff_SSB4.png",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        "name":"Fox",
+        "photo":"https://www.ssbwiki.com/images/a/a6/Jigglypuff_SSB4.png",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    }
+]
