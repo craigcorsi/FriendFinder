03ceba0828ba364a4aa29cdfbbb97081274c8a85
Craig <corsi007@umn.edu>
2018-07-03 17:15:33 -0500

    client-side event-handling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -8,18 +8,18 @@ var path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static("app/public"));
+
 var friends = require('./app/data/friends.js');
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./app/public/home.html"));
 });
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
-});
-
 app.get("/:route", function (req, res) {
-    if (req.params.route != "survey") {
+    if (req.params.route == "survey") {
+        res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+    } else {
         res.sendFile(path.join(__dirname, "./app/public/home.html"));
     }
 });
@@ -30,17 +30,30 @@ app.get("/api/friends", function (req, res) {
 
 // Displays a single character, or returns false
 app.post("/api/friends", function (req, res) {
-    console.log(req);
-    var bestDiff = totalDifference(req, friends[0]);
+    var newFriend = req.body;
+    // determine the closest matching friend
+    var bestDiff = totalDifference(newFriend, friends[0]);
     var positionOfBest = 0;
     for (j = 1; j < friends.length; i++) {
-        var temp = totalDifference(req, friends[j]);
+        var temp = totalDifference(newFriend, friends[j]);
         if (temp < bestDiff) {
             bestDiff = temp;
             positionOfBest = j;
         }
     }
-    return res.json(friends[positionOfBest]);
+
+    // add new friend to the list
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriend.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newFriend);
+    friends.push(newFriend);
+    // res.json(newFriend);
+
+
+    // Now return the closest matching friend
+    res.json(friends[positionOfBest]);
 });
 
 function totalDifference(char1, char2) {
