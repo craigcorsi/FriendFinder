2574d008e088fe3663cd6e10ac1b890b5f67c9c4
Craig <corsi007@umn.edu>
2018-07-23 14:36:19 -0500

    fix post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,38 +3,6 @@
 var friends = require('../data/friends.js');
 
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        return res.json(friends);
-    });
-
-    // Displays a single character, or returns false
-    app.post("/api/friends", function (req, res) {
-        var newFriend = req.body;
-        // determine the closest matching friend
-        var bestDiff = totalDifference(newFriend, friends[0]);
-        var positionOfBest = 0;
-        for (j = 1; j < friends.length; i++) {
-            var temp = totalDifference(newFriend, friends[j]);
-            if (temp < bestDiff) {
-                bestDiff = temp;
-                positionOfBest = j;
-            }
-        }
-
-        // add new friend to the list
-
-        // Using a RegEx Pattern to remove spaces from newCharacter
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        newFriend.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-        console.log(newFriend);
-        friends.push(newFriend);
-        // res.json(newFriend);
-
-
-        // Now return the closest matching friend
-        res.json(friends[positionOfBest]);
-    });
-
     function totalDifference(char1, char2) {
         var scores1 = char1.scores;
         var scores2 = char2.scores;
@@ -44,4 +12,37 @@ module.exports = function (app) {
         }
         return total;
     }
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+    // Displays a single character, or returns false
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+
+        // determine the closest matching friend
+        var bestDiff = totalDifference(newFriend, friends[0]);
+        console.log(bestDiff);
+        var positionOfBest = 0;
+        for (j = 1; j < friends.length; j++) {
+            var temp = totalDifference(newFriend, friends[j]);
+            console.log(temp);
+            if (temp < bestDiff) {
+                bestDiff = temp;
+                positionOfBest = j;
+            }
+        }
+
+        // add new friend to the list
+        console.log(newFriend);
+        friends.push(newFriend);
+        // res.json(newFriend);
+
+
+        // Now return the closest matching friend
+        res.json(friends[positionOfBest]);
+    });
+
+    
 }
